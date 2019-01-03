5dda4b7327044d6b03c28543613db16bcd26a684
Justin Chan <justin.the.c@gmail.com>
2019-01-03 01:53:17 -0500

    Change description column to use TEXT instead of VARCHAR(255)

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 03, 2019 at 06:28 AM
+-- Generation Time: Jan 03, 2019 at 06:50 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -35,7 +35,7 @@ CREATE TABLE `item` (
   `product_code` tinytext NOT NULL,
   `image_url` text NOT NULL,
   `name` tinytext NOT NULL,
-  `description` varchar(255) DEFAULT NULL
+  `description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Product items for price tracking';
 
 --
