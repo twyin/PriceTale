c9f66efb2490318561da09a64dbf67fcf08ae548
Justin Chan <justin.the.c@gmail.com>
2019-01-03 22:28:03 -0500

    Filled in New Item Page

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ $conn->close();
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">PriceTale</a>
+    <a class="navbar-brand" href="/">PriceTale</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -70,7 +70,7 @@ $conn->close();
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="/">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Brands</a>
@@ -79,6 +79,9 @@ $conn->close();
           <a class="nav-link" href="#">Categories</a>
         </li>
       </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <a href="items/new.php" class="btn btn-outline-primary my-2 my-sm-0" role="button">Add Item</a>
+      </form>
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search Items" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
